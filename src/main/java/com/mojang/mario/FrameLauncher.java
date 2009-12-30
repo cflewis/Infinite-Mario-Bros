7c212fae1814848bac98ba5b58fd740c6ddbd217
Chris Lewis <chris@chris.to>
2009-12-29 23:39:50 -0800

    Changed title, POM can now generate self-executing JAR

--- src/main/java/com/mojang/mario/FrameLauncher.java
+++ src/main/java/com/mojang/mario/FrameLauncher.java
@@ -8,7 +8,7 @@ public class FrameLauncher
     public static void main(String[] args)
     {
         MarioComponent mario = new MarioComponent(640, 480);
-        JFrame frame = new JFrame("Mario Test");
+        JFrame frame = new JFrame("Infinite Mario Bros.");
         frame.setContentPane(mario);
         frame.pack();
         frame.setResizable(false);
